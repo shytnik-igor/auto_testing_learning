3eb96f227ace3fe68c6b7d66114718b794bfdaf3
marina <marusya1289@gmail.com>
2022-09-29 16:05:01 +0300

    Created test

--- src/test/kotlin/goods/GoodsTest.kt
+++ src/test/kotlin/goods/GoodsTest.kt
@@ -1,5 +1,6 @@
 package goods
 
+import com.codeborne.selenide.Condition
 import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.selector.ByTagAndText
 import org.junit.jupiter.api.BeforeAll
@@ -12,7 +13,7 @@ class GoodsTest {
     @BeforeAll
     fun setUp() {
         // TODO вписать, какую страницу открывать
-        Selenide.open("https://google.com/ncr")
+        Selenide.open("https://www.21vek.by/diapers/pants5junior_pampers_03.html")
     }
 
     @Test
@@ -20,11 +21,14 @@ class GoodsTest {
         // TODO Реализовать шаги тест-кейса
         // Найти кнопку "В корзину”
         val addToCartButton = Selenide.element(ByTagAndText("button", "В корзину"))
+        addToCartButton.shouldBe(Condition.visible)
         // Проверить, что не отображается кнопка “В корзине”
-
+        val addInCartButton = Selenide.element(ByTagAndText("a", "В корзине"))
+        addInCartButton.shouldNot(Condition.visible)
         // Нажать кнопку “В корзину”
-
+        addToCartButton.click()
         // Проверить, что кнопка изменилась на “В корзине”
-
+        addToCartButton.shouldNot(Condition.visible)
+        addInCartButton.shouldBe(Condition.visible)
     }
 }

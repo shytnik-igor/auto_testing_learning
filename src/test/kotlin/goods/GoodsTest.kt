352a4d2f030798fc7e389bfb690c0b30089b2002
marina <marusya1289@gmail.com>
2022-10-03 12:57:40 +0300

    Created test

--- src/test/kotlin/goods/GoodsTest.kt
+++ src/test/kotlin/goods/GoodsTest.kt
@@ -12,23 +12,21 @@ class GoodsTest {
 
     @BeforeAll
     fun setUp() {
-        // TODO вписать, какую страницу открывать
         Selenide.open("https://www.21vek.by/diapers/pants5junior_pampers_03.html")
     }
 
     @Test
     fun notUsingDollars() {
-        // TODO Реализовать шаги тест-кейса
         // Найти кнопку "В корзину”
         val addToCartButton = Selenide.element(ByTagAndText("button", "В корзину"))
         addToCartButton.shouldBe(Condition.visible)
         // Проверить, что не отображается кнопка “В корзине”
-        val addInCartButton = Selenide.element(ByTagAndText("a", "В корзине"))
-        addInCartButton.shouldNot(Condition.visible)
+        val inCartLink = Selenide.element(ByTagAndText("a", "В корзине"))
+        inCartLink.shouldNot(Condition.visible)
         // Нажать кнопку “В корзину”
         addToCartButton.click()
         // Проверить, что кнопка изменилась на “В корзине”
         addToCartButton.shouldNot(Condition.visible)
-        addInCartButton.shouldBe(Condition.visible)
+        inCartLink.shouldBe(Condition.visible)
     }
 }

38d9cfdfc06b362e8a2e6f168992420dbfa25db6
marina <marusya1289@gmail.com>
2022-10-04 11:36:22 +0300

    Moved locators from the GoodsTest and implemented methods

--- src/test/kotlin/page/GoodsPage.kt
+++ src/test/kotlin/page/GoodsPage.kt
@@ -1,7 +1,31 @@
 package page
 
+import com.codeborne.selenide.Condition
+import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.selector.ByTagAndText
+
 class GoodsPage {
 
-    // TODO Перенести сюда локаторы из теста GoodsTest и реализовать методы
+    val addToCartButton = Selenide.element(ByTagAndText("button", "В корзину"))
+    val inCartLink = Selenide.element(ByTagAndText("a", "В корзине"))
 
+    fun assertAddToCartButtonVisible() {
+        addToCartButton.shouldBe(Condition.visible)
+    }
+
+    fun clickAddToCartButton() {
+        addToCartButton.click()
+    }
+
+    fun assertAddToCartButtonInvisible() {
+        addToCartButton.shouldNot(Condition.visible)
+    }
+
+    fun assertInCartLinkInvisible() {
+        inCartLink.shouldNot(Condition.visible)
+    }
+
+    fun assertInCartLinkVisible() {
+        inCartLink.shouldBe(Condition.visible)
+    }
 }
